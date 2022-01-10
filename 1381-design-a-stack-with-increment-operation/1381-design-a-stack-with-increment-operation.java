92cde7fff490bef477257bb1231a02b497a39c98
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-01-10 18:02:39 +0530

    Time: 4 ms (97.05%), Space: 40.3 MB (52.30%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,26 +1,33 @@
 class CustomStack {
-    int []stack;
-    int top;
+    int []inc;
+    int n;
+    Stack<Integer> s;
     public CustomStack(int maxSize) {
-        stack = new int[maxSize];
-        top=-1;
+        inc = new int[maxSize];
+        n=maxSize;
+        s= new Stack<>();
     }
     
     public void push(int x) {
-        if(top<stack.length-1)
-            stack[++top] = x;
+        if(s.size()<n)
+            s.push(x);
     }
     
     public int pop() {
-        if(top>=0)
-            return stack[top--];
-        return -1;
+        int i=s.size()-1;
+        if(i<0)
+            return -1;
+        if(i>0)
+            inc[i-1]+=inc[i];
+        int res = s.pop() + inc[i];
+        inc[i]=0;
+        return res;
     }
     
     public void increment(int k, int val) {
-        for(int i=0;i<k && i<stack.length;i++){
-            stack[i]=stack[i]+val;
-        }
+        int i=Math.min(k,s.size())-1;
+        if(i>=0)
+            inc[i]+=val;
     }
 }
 
