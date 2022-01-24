c2f82a736f6c6b811ecc2a186f554654bdc5c3d8
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-01-24 13:55:32 +0530

    Time: 1 ms (46.74%), Space: 38.5 MB (48.62%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,21 +14,15 @@
  * }
  */
 class Solution {
-    TreeNode bstFromPreorder(int[] pre, int ps, int pe, int[] in, int is, int ie, Map<Integer, Integer> mapIn){
-        if(ps>pe || is>ie) return null;
-        TreeNode root = new TreeNode(pre[ps]);
-        int inRoot = mapIn.get(root.val);
-        int inLeft = inRoot - is;
-        root.left = bstFromPreorder(pre, ps+1,ps+inLeft, in, is, inRoot-1, mapIn);
-        root.right = bstFromPreorder(pre, ps+inLeft+1, pe, in, inRoot+1, ie, mapIn);
+    public TreeNode bst(int pre[], int upbound, int[] i){
+        
+        if(pre.length==i[0] || upbound<pre[i[0]]) return null;
+        TreeNode root = new TreeNode(pre[i[0]++]);
+        root.left = bst(pre, root.val, i);
+        root.right = bst(pre, upbound, i);
         return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
-        int inorder[] = new int[preorder.length];
-        inorder =preorder.clone();
-        Arrays.sort(inorder);
-        Map<Integer, Integer> mapIn = new HashMap<>();
-        for(int i=0;i<inorder.length;i++) mapIn.put(inorder[i],i);
-        return bstFromPreorder(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1, mapIn);
+        return bst(preorder,Integer.MAX_VALUE,new int[]{0});
     }
 }
