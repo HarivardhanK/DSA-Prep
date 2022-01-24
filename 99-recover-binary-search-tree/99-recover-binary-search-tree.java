eb9d39393cddd1ae8a146cc856703449ad8920a6
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-01-24 20:29:29 +0530

    Time: 4 ms (39.29%), Space: 44.3 MB (36.09%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // here middle is first wrongly placed node 
+    // first is the first prev
+    // last is the second wrongly placed node
+    //prev stores previous element
+    TreeNode prev, first, middle, last;
+    public void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        if(prev!=null && root.val<prev.val){
+            if(first==null){
+                first=prev;
+                middle=root;
+            }
+            else last = root;
+        }
+        //mark this node as previous
+        prev=root;
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        first = middle = last =null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        if(first!=null && last!=null){
+            int temp = first.val;
+            first.val= last.val;
+            last.val=temp;
+        }
+        else if(first!=null && middle!=null){
+            int temp = first.val;
+            first.val= middle.val;
+            middle.val=temp;
+        }
+    }
+}
