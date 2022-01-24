0d7cb2b1fce375d91939966f6f8b5bf886cc2208
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-01-24 13:04:24 +0530

    Time: 13 ms (5.95%), Space: 46.9 MB (36.51%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -11,12 +11,13 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return null;
-        if(root==p || root==q) return root;
+        
         if(root.val>p.val && root.val>q.val)
             return lowestCommonAncestor(root.left, p,q);
         else if(root.val<p.val && root.val<q.val)
             return lowestCommonAncestor(root.right, p,q);
-        else 
-            return root;
+        // if p.val == root.val || q.val ==root.val
+        // if p and q are dividing like p is on left and q is on right or viceversa
+        return root;
     }
 }
