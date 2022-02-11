90d2c72f7fe633fd6efefa9eadf00a966e1e4a60
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-02-11 20:54:03 +0530

    Time: 331 ms (5.42%), Space: 250.6 MB (5.42%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.java
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.java
@@ -1,23 +1,8 @@
 class Solution {
     public String largestMerge(String word1, String word2) {
-        StringBuilder merge = new StringBuilder();
-        int i=0,j=0;
-        int diff = word1.compareTo(word2);
-        while(i<word1.length() && j<word2.length()){
-            diff = word1.substring(i).compareTo(word2.substring(j));
-            if(word1.charAt(i)>word2.charAt(j)){
-                merge.append(word1.charAt(i++));
-            }
-            else if(word1.charAt(i)<word2.charAt(j)){
-                merge.append(word2.charAt(j++));
-            }
-            else{
-                if(diff>0) merge.append(word1.charAt(i++));
-                else merge.append(word2.charAt(j++));
-            }
-        }
-        if(i<word1.length()) merge.append(word1.substring(i));
-        if(j<word2.length()) merge.append(word2.substring(j));
-        return merge.toString();
+        if(word1.length()==0 || word2.length()==0) return word1+word2;
+        if(word1.compareTo(word2)>0) 
+            return word1.charAt(0)+largestMerge(word1.substring(1),word2);
+        return word2.charAt(0)+largestMerge(word1,word2.substring(1));
     }
 }
