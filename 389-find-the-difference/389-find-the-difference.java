ca34ec055fb18d144f679ceddf7750f680dbac34
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-13 22:52:01 +0530

    Time: 1 ms (99.83%), Space: 41.7 MB (72.28%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,17 +1,8 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        if(s.length()==0) return t.charAt(0);
-        int i=0;
-        char sarr[] = s.toCharArray();
-        Arrays.sort(sarr);
-        s= new String(sarr);
-        char tarr[] = t.toCharArray();
-        Arrays.sort(tarr);
-        t= new String(tarr);
-        while(i<s.length()){
-            if(sarr[i]!=tarr[i]) break;
-            i++;
-        }
-        return tarr[i];
+       char c=0;
+        for(char ch:s.toCharArray()) c^=ch;
+        for(char ch:t.toCharArray()) c^=ch;
+        return c;
     }
 }
