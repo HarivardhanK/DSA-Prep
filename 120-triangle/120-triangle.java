81fa50f5a54eb98525be026e4820f6bac8dba3d3
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-19 18:13:25 +0530

    Time: 2 ms (93.64%), Space: 44.4 MB (28.99%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,23 +1,17 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for(int r=1;r<triangle.size();r++){
-            for(int c=0;c<triangle.get(r).size();c++){
-                if(c==0){ 
-                    triangle.get(r).set(c,triangle.get(r).get(c)+triangle.get(r-1).get(c));
-                    continue;    
-                }
-                if(c==triangle.get(r).size()-1){
-                    triangle.get(r).set(c,triangle.get(r).get(c)+triangle.get(r-1).get(c-1));
-                    continue;
-                }
-                int minnum=Math.min(triangle.get(r-1).get(c-1),triangle.get(r-1).get(c));
-                triangle.get(r).set(c,triangle.get(r).get(c)+minnum);
+        // https://takeuforward.org/data-structure/minimum-path-sum-in-triangular-grid-dp-11/
+        int n=triangle.size();
+        int prev[] = new int[n];
+        int curr[] = new int[n];
+        //filling the last into the prev array
+        for(int j=0;j<n;j++) prev[j] = triangle.get(n-1).get(j);
+        for(int r=n-2;r>=0;r--){
+            for(int c=0;c<=r;c++){
+                curr[c] = triangle.get(r).get(c) + Math.min(prev[c],prev[c+1]);
             }
+            prev=curr;
         }
-        int min=Integer.MAX_VALUE;
-        for(int c=0;c<triangle.get(triangle.size()-1).size();c++){
-            min=Math.min(min,triangle.get(triangle.size()-1).get(c));
-        }
-        return min;
+        return prev[0];
     }
 }
