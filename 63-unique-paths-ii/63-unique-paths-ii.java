171c7592736b5e52476fb29623f3a18ce47b25e7
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-19 14:24:36 +0530

    Time: 1 ms (57.11%), Space: 41.8 MB (60.02%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int paths(int r, int c, boolean grid[][] , int obsgrid[][], int dp[][]){
-        if(r<0 && c<0) return 0;
-        if(grid[r][c] || obsgrid[r][c]==1) return 0;
-        if(r==0 && c==0) return 1;
-        if(dp[r][c]==0){
-            grid[r][c]=true;
-            int count=0;
-            if(r-1>=0)
-                count+=paths(r-1,c,grid,obsgrid,dp);
-            if(c-1>=0)
-                count+=paths(r,c-1,grid,obsgrid,dp);
-            grid[r][c]=false;
-            dp[r][c]=count;
-        }
-        return dp[r][c];
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if(obstacleGrid.length==1 && obstacleGrid[0].length ==1) return obstacleGrid[0][0]^1;
-        int dp[][] = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for(int row[]:dp)
-            Arrays.fill(row,0);
-        boolean grid[][] = new boolean[obstacleGrid.length][obstacleGrid[0].length];
-        paths(obstacleGrid.length-1,obstacleGrid[0].length-1,grid,obstacleGrid,dp);
-        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        if(obstacleGrid[0][0]==1) return 0;
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int prev[]=new int[n];
+        for(int r=0;r<m;r++){
+            int curr[] = new int[n];
+            for(int c=0;c<n;c++){
+                if(obstacleGrid[r][c]==1){
+                    curr[c]=0;
+                    continue;
+                }
+                if(r==0 && c==0){
+                    curr[c]=1;
+                    continue;
+                }
+                curr[c]=0;
+                if(r>0)
+                    curr[c]+=prev[c];
+                if(c>0)
+                    curr[c]+=curr[c-1];
+            }
+            prev=curr;
+        }
+        return prev[n-1];
     }
 }
