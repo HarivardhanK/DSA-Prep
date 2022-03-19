a3d201c79d8ae2f5b4676a43dd8ccf7b600ae6ba
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-19 12:36:40 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (70.94%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,28 +1,20 @@
 class Solution {
-    public int totalpaths(int r, int c, boolean grid[][], int dp[][]){
+    public int totalpaths(int r, int c, int dp[][]){
         if(r==0 && c==0) return 1;
         if(r<0 || c<0) return 0;
-        if(grid[r][c]) return 0;
         if(dp[r][c]==-1){ 
-            int count=0;           
-            grid[r][c]=true;
-            if(r-1>=0){
-                count+=totalpaths(r-1,c,grid, dp);
-            }
-            if(c-1>=0){
-                count+=totalpaths(r,c-1,grid, dp);
-            }
-            grid[r][c]=false;
+            int count=0;
+            count+=totalpaths(r-1,c, dp);
+            count+=totalpaths(r,c-1, dp);
             dp[r][c]=count;
         }
         return dp[r][c];
     }
     public int uniquePaths(int m, int n) {
         if(m==1 && n==1) return 1;
-        boolean grid[][] = new boolean[m][n];
         int dp[][] = new int[m][n];
         for(int row[]:dp) Arrays.fill(row,-1);
-        totalpaths(m-1,n-1,grid,dp);
+        totalpaths(m-1,n-1,dp);
         return dp[m-1][n-1];
     }
 }
