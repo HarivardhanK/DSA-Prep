b4e91969c60911abd4b718dabae83b75ffb87323
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-22 11:18:52 +0530

    Time: 2 ms (60.42%), Space: 50 MB (36.19%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0,j=(numbers.length-1);
         while(i<j){
             if(numbers[i]+numbers[j]==target) return new int[]{i+1,j+1};
-            else if(i<j && numbers[i]+numbers[j]<target) i++;
+            else if(numbers[i]+numbers[j]<target) i++;
             else j--;
         }
         return new int[]{};
