052b99a7d4e2284845ced7532c52366622967303
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-22 20:36:05 +0530

    Time: 40 ms (82.81%), Space: 50 MB (93.37%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.java
+++ 901-online-stock-span/901-online-stock-span.java
@@ -1,18 +1,14 @@
 class StockSpanner {
-    Stack<Integer> stk;
-    ArrayList<Integer> arr;
+    Stack<Integer[]> stk;
     public StockSpanner() {
         stk = new Stack<>();
-        arr = new ArrayList<>();
     }
-    
     public int next(int price) {
-        while(stk.size()>0 && arr.get(stk.peek())<=price) stk.pop();
-        arr.add(price);
-        int ans =  (stk.size()>0)?arr.size()-stk.peek()-1:1;
-        if(stk.size()==0 && arr.size()>1) ans = arr.size();
-        stk.push(arr.size()-1);
-        return ans;
+        int res=1;
+        while(stk.size()>0 && stk.peek()[0]<=price) 
+            res+=stk.pop()[1];
+        stk.push(new Integer[]{price,res});
+        return res;
     }
 }
 
