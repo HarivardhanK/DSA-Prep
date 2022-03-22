663486ce0d638b6d4d0885a96bb9dd75d98df8e7
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-22 12:33:41 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (29.84%) - LeetHub

--- 86-partition-list/86-partition-list.java
+++ 86-partition-list/86-partition-list.java
@@ -10,37 +10,23 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode le=null,lehead=null;
-        ListNode ge=null,gehead=null;
-        ListNode temp=head;
-        while(temp!=null){
-            if(temp.val<x){
-                if(lehead==null){
-                    lehead=temp;
-                    le=temp;
-                }
-                else{
-                    le.next=temp;
-                    le=temp;
-                }
-                temp=temp.next;
-                le.next=null;
+        ListNode beforehead = new ListNode(0);
+        ListNode before = beforehead;
+        ListNode afterhead = new ListNode(0);
+        ListNode after = afterhead;
+        while(head!=null){
+            if(head.val<x){
+                before.next=head;
+                before=head;
             }
             else{
-                if(gehead==null){
-                    gehead=temp;
-                    ge=temp;
-                }
-                else{
-                    ge.next=temp;
-                    ge=temp;
-                }
-                temp=temp.next;
-                ge.next=null;
+                after.next=head;
+                after=head;
             }
+            head=head.next;
         }
-        if(lehead==null) return gehead;
-        else le.next=gehead;
-        return lehead;
+        after.next=null;
+        before.next=afterhead.next;
+        return beforehead.next;
     }
 }
