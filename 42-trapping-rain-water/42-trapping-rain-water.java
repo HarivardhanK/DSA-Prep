bf9a3c220afd5b61929ce8aa30d4521a4e3d56ca
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-03-23 13:58:37 +0530

    Time: 2 ms (49.04%), Space: 46.6 MB (48.19%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,22 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int n=height.length;
-        int maxr[] = new int[n];
-        int maxl[] = new int[n];
-        maxl[0]=height[0];
-        maxr[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            maxl[i]=Math.max(maxl[i-1],height[i]);
-            if(n-i>0)
-            maxr[n-i-1]=Math.max(maxr[n-i],height[n-i-1]);
-        }
-        int area[] = new int[n];
-        for(int i=0;i<n;i++){
-            area[i]=Math.min(maxl[i],maxr[i])-height[i];
-        }
+        //Two pointer appraoch
+        int l=0,r=height.length-1;
+        int lmax=height[l],rmax=height[r];
         int ans=0;
-        for(int i=0;i<n;i++){
-            if(area[i]>0) ans+=area[i];
+        while(l<r){
+            if(height[l]<height[r]){
+                if(height[l]>=lmax) lmax=height[l];
+                else ans+=(lmax-height[l]);
+                l++;
+            }
+            else{
+                if(height[r]>=rmax) rmax=height[r];
+                else ans+=(rmax-height[r]);
+                r--;
+            }
         }
         return ans;
     }
