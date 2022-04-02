49ad45411286abf13b6c357843cd2ead606ccc8a
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-02 19:46:22 +0530

    Time: 84 ms (34.63%), Space: 102.9 MB (16.39%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -4,16 +4,13 @@ class Solution {
         int max=0;
         for(int i=0,j=0;j<fruits.length;j++){
             map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
-            if(map.size()<=2){
-                max=Math.max(max,j-i+1);
+            while(map.size()>2){
+                map.put(fruits[i],map.get(fruits[i])-1);
+                if(map.get(fruits[i])==0) map.remove(fruits[i]);
+                i++;
             }
-            else{
-                while(map.size()>2){
-                    map.put(fruits[i],map.get(fruits[i])-1);
-                    if(map.get(fruits[i])==0) map.remove(fruits[i]);
-                    i++;
-                }
-            }
+            max=Math.max(max,j-i+1);
+
         }
         return max;
     }
