5c6f1ae47183a77d5a698eae6bbcae97f60e592a
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-10 20:40:58 +0530

    Time: 31 ms (52.05%), Space: 47.2 MB (40.82%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,28 +1,30 @@
 class Solution {
-    public int f(int[][] grid, int r,int c1, int c2,int dp[][][]){
-        if(r>=grid.length || c1<0 || c2<0||( c1>=grid[0].length || c2>=grid[0].length) ){
-            return Integer.MIN_VALUE;
-        }
-        if(dp[r][c1][c2]!=0) return dp[r][c1][c2];
-        int cherries = 0;
-        if(c1==c2) cherries+=grid[r][c1];
-        else cherries+=grid[r][c1] + grid[r][c2];
-        
-        if(r==grid.length-1){
-            return cherries;
-        }
-        int max=0;
-        for(int col1=c1-1;col1<=c1+1;col1++){
-            for(int col2=c2-1;col2<=c2+1;col2++){
-                max=Math.max(max,f(grid,r+1,col1,col2,dp));
-            }
-        }
-        dp[r][c1][c2]=cherries+max;
-        return cherries+max;
-    }
     public int cherryPickup(int[][] grid) {
         int n=grid.length,m=grid[0].length;
-        int dp[][][] = new int[n][m][m];
-        return f(grid,0,0,grid[0].length-1,dp);
+        int prev[][] = new int[m][m];
+        for(int row = n-1;row>=0;row--){
+            int curr[][] = new int[m][m];
+            for(int col1=0;col1<m;col1++){
+                for(int col2=0;col2<m;col2++){
+                    int res = 0;
+                    res+=grid[row][col1];
+                    if(col1!=col2) res+=grid[row][col2];
+                    if(row!=n-1){
+                        int max=0;
+                        for(int c1=col1-1;c1<=col1+1;c1++){
+                            for(int c2=col2-1;c2<=col2+1;c2++){
+                                if(c1>=0 && c2>=0 && c1<m && c2<m){
+                                    max=Math.max(max,prev[c1][c2]);
+                                }
+                            }
+                        }
+                        res+=max;
+                    }
+                    curr[col1][col2] = res;
+                }
+            }
+            prev=curr;
+        }
+        return prev[0][m-1];
     }
 }
