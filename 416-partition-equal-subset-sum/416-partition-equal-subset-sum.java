1cd2246ca3bee4720acda6541862551160482ec7
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-13 13:26:34 +0530

    Time: 38 ms (68.93%), Space: 42.4 MB (87.71%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,24 +1,29 @@
 class Solution {
-    boolean cp(int[] nums,int i,int s,int[][] dp){
-        if(s==0) return true;
-        if(i==0) return nums[0]==s;
-        if(dp[i][s]!=-1) return dp[i][s]==0?false:true;
-        boolean nopick = cp(nums,i-1,s,dp);
-        boolean pick = false;
-        if(nums[i]<=s)
-            pick = cp(nums,i-1,s-nums[i],dp);
-        dp[i][s]=nopick||pick?1:0;
-        return nopick||pick;
-    }
     public boolean canPartition(int[] nums) {
         int sum=0,n=nums.length;
         for(int ele:nums) sum+=ele;
-        if((sum&1)!=0) return false;//if odd we can't divide into two
-        int[][] dp = new int[n][(sum/2)+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        
+        if((sum&1)==1) return false;//if odd we can't divide into two
+        int s=sum/2;
+        boolean[] prev = new boolean[s+1];
         //if we can get one subset with sum of totalsum/2 then
         //we can partition the array into two
-        return cp(nums,n-1,sum/2,dp);
+        //if(s==0) fill true
+        prev[0] = true;
+        if(nums[0]<=s) prev[nums[0]]=true;
+        for(int i=1;i<n;i++){
+            boolean[] curr = new boolean[s+1];
+            curr[0] = true;
+            for(int j=1;j<=s;j++){
+                boolean nopick=prev[j];
+                
+                boolean pick=false;
+                if(nums[i]<=j)
+                    pick=prev[j-nums[i]];
+                
+                curr[j]=nopick||pick;
+            }
+            prev=curr;
+        }
+        return prev[s];
     }
 }
