0f7d18bfa8c5117f172bb8ce2bfc7f89b5fbdd99
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-19 22:43:50 +0530

    Time: 36 ms (21.10%), Space: 51 MB (7.12%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,21 +1,25 @@
 class Solution {
-    private int nd(String s, String t, int i, int j,int dp[][]){
-        //base case
-        if(j==0) return 1;
-        if(i==0) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        //exploring all possible cases
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            return dp[i][j]=nd(s,t,i-1,j-1,dp)+nd(s,t,i-1,j,dp);
-        }
-        return dp[i][j] = nd(s,t,i-1,j,dp);
-    }
     public int numDistinct(String s, String t) {
         int n=s.length(),m=t.length();
-        int dp[][] = new int[n+1][m+1];
-        for(int row[]:dp)
-            Arrays.fill(row,-1);
-        return nd(s,t,n,m,dp);
+        //considering the 1 based indexing 
+        //since our base case can go negative start from 1 
+        
+        int prev[] = new int[m+1];
+        prev[0] = 1;
+   
+        for(int i=1;i<=n;i++){
+            int curr[] = new int[m+1];
+            curr[0] = 1;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    curr[j] = prev[j-1] + prev[j];
+                }
+                else{
+                    curr[j] = prev[j];
+                }
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
 }
