00973fb3f4c66f7bc4dfafdddc893dc6bd36087b
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-19 22:45:00 +0530

    Time: 23 ms (60.93%), Space: 42.5 MB (92.27%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -8,17 +8,14 @@ class Solution {
         prev[0] = 1;
    
         for(int i=1;i<=n;i++){
-            int curr[] = new int[m+1];
-            curr[0] = 1;
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    curr[j] = prev[j-1] + prev[j];
+                    prev[j] = prev[j-1] + prev[j];
                 }
                 else{
-                    curr[j] = prev[j];
+                    prev[j] = prev[j];
                 }
             }
-            prev=curr;
         }
         return prev[m];
     }
