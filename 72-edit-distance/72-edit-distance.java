308a9c8bbe8a798413fbb6ddd787919eee99cc1d
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-20 20:25:30 +0530

    Time: 9 ms (42.25%), Space: 44.7 MB (60.96%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,18 +1,22 @@
 class Solution {
-    private int md(String s, String t, int i, int j, int dp[][]){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(s.charAt(i-1)==t.charAt(j-1))
-            return dp[i][j] = md(s,t,i-1,j-1,dp);
-        // insert, delete and replace
-        return dp[i][j] = 1+Math.min(md(s,t,i,j-1,dp),Math.min(md(s,t,i-1,j,dp),md(s,t,i-1,j-1,dp)));
-    }
     public int minDistance(String s, String t) {
         int n=s.length(),m=t.length();
-        int dp[][] = new int[n+1][m+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return md(s,t,n,m,dp);
+        int prev[] = new int[m+1];
+        for(int j=0;j<=m;j++) prev[j] = j;
+        
+        for(int i=1;i<=n;i++){
+            int curr[] = new int[m+1];
+            curr[0] = i;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    curr[j] = prev[j-1];
+                }
+                else{
+                    curr[j] = 1+Math.min(prev[j],Math.min(curr[j-1],prev[j-1]));
+                }
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
 }
