7fea9fb1d2b3a05152550b930a3cb71d9eb95a2d
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-21 20:35:42 +0530

    Time: 2 ms (42.86%), Space: 42.1 MB (56.92%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,25 +1,20 @@
 class Solution {
+    private int mp(int[] prices,int ind,int buy, int[][] dp){
+        if(ind>=prices.length) return 0;
+        
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        if(buy==1){
+            return dp[ind][buy]=Math.max(-prices[ind]+mp(prices,ind+1,0,dp),mp(prices,ind+1,1,dp));
+        }
+        else{
+            return dp[ind][buy]=Math.max(prices[ind]+mp(prices,ind+2,1,dp),mp(prices,ind+1,0,dp));
+        }
+    }
     public int maxProfit(int[] prices) {
         //sell-0, buy-1 , cooldown - 2 
         int n=prices.length;
-        int []ahead = new int[2];
-        int [] ahead1 = new int[2];
-        
-        for(int ind=n-1;ind>=0;ind--){
-            int []curr = new int[2];
-            for(int j=0;j<=1;j++){
-                if(j==1){
-                    curr[j]=Math.max(-prices[ind]+ahead[0],ahead[1]);
-                }
-                else{
-                    int smallprofit=0;
-                    if(ind+2<=n) smallprofit+=ahead1[1];
-                    curr[j]= Math.max(prices[ind]+smallprofit,ahead[0]);
-                }
-            }
-            ahead1=ahead;
-            ahead=curr;
-        }
-        return ahead[1];   
+        int [][]dp = new int[n][2];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return mp(prices,0,1,dp);   
     }
 }
