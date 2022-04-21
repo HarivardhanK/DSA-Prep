7721d7d195bc8e309152d8699e5264d04598983a
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-21 18:10:51 +0530

    Time: 64 ms (19.81%), Space: 104.2 MB (16.20%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,19 +1,4 @@
 class Solution {
-    private int mp(int[] prices,int ind,int noOfTrans,int dp[][] ){
-        if(noOfTrans>4 || ind==prices.length) return 0;
-        
-        if(dp[ind][noOfTrans]!=-1) return dp[ind][noOfTrans];
-        int profit=0;
-        if(noOfTrans%2==0){
-            profit=Math.max(-prices[ind]+mp(prices,ind+1,noOfTrans+1,dp),
-                           mp(prices,ind+1,noOfTrans,dp));
-        }
-        else{
-            profit=Math.max(prices[ind]+mp(prices,ind+1,noOfTrans+1,dp),
-                           mp(prices,ind+1,noOfTrans,dp));
-        }
-        return dp[ind][noOfTrans]=profit;
-    }
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int dp[][] = new int[n+1][5];
