18cfbc480b095be046ab51215f3376ce32301bd3
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-04-29 10:05:17 +0530

    Time: 1 ms (83.33%), Space: 43.2 MB (77.89%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -11,6 +11,21 @@ class Solution {
         }
         return true;
     }
+    private boolean isbiparbfs(int i, int[] colors, int[][] graph){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(i);
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int it:graph[node]){
+                if(colors[it]==-1){
+                    colors[it] = 1-colors[node];
+                    q.offer(it);
+                }
+                else if(colors[it]==colors[node]) return false;
+            }
+        }
+        return true;
+    }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int colors[] = new int[n];
@@ -19,7 +34,7 @@ class Solution {
         for(int i=0;i<n;i++){
             if(colors[i]==-1){
                 colors[i] = 1;
-                if(!isbipardfs(i,colors,graph)) return false;
+                if(!isbiparbfs(i,colors,graph)) return false;
             }
         }
         return true;
