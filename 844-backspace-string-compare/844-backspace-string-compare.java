ef028a02f9874e495fb5499048c64f4e8aaad5c1
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-01 16:45:58 +0530

    Time: 1 ms (87.56%), Space: 41.4 MB (73.19%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,17 +1,31 @@
 class Solution {
-    public String getString(String str){
-        Stack<Character> stk = new Stack<>();
-        for(char c:str.toCharArray()){
-            if(c!='#'){
-                stk.push(c);
-            }
-            else if(!stk.empty()){
-                stk.pop();
-            }
-        }
-        return String.valueOf(stk);
-    }
     public boolean backspaceCompare(String s, String t) {
-        return getString(s).equals(getString(t));
+        int i=s.length()-1, j=t.length()-1;
+        int skipi=0,skipj=0;
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    skipi++; i--;
+                }
+                else if(skipi>0){
+                    skipi--; i--;
+                }
+                else break;
+            }
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    skipj++; j--;
+                }
+                else if(skipj>0){
+                    skipj--; j--;
+                }
+                else break;
+            }
+            if(i>=0 && j>=0 && s.charAt(i)!=t.charAt(j)) return false;
+            
+            if(i>=0 != j>=0) return false;
+            i--;j--;
+        }
+        return true;
     }
 }
