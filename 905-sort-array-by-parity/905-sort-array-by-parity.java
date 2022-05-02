de33f6771869b76eedd654bee1f25807a7cc3ae9
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-02 19:23:19 +0530

    Time: 1 ms (96.61%), Space: 48 MB (61.99%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,16 +1,18 @@
 class Solution {
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public int[] sortArrayByParity(int[] nums) {
+        //inplace algorithm
         int i=0,j=nums.length-1;
         while(i<j){
             while(i<nums.length && nums[i]%2==0) i++;
             while(j>=0 && nums[j]%2!=0) j--;
-            if(i>=nums.length || j<=0 || i>=j) break;
-            if(nums[i]%2!=0 && nums[j]%2==0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;j--;
-            }
+            if(i==nums.length || j<0 || i>j) break;
+            swap(nums,i,j);
+            i++;j--;
         }
         return nums;
     }
