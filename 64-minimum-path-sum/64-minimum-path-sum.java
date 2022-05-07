4459b952057c73f9900c52ade7a573c87c23a6ab
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-07 17:53:22 +0530

    Time: 2 ms (87.45%), Space: 45.7 MB (61.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,27 +1,18 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        //space optimised
-        int prev[]= new int[grid[0].length];
-        for(int r=0;r<grid.length;r++){
-            int curr[]= new int[grid[0].length];
-            for(int c=0;c<grid[0].length;c++){
-                if(r==0 && c==0){
-                    curr[c]=grid[r][c];
-                    continue;
-                }
-                if(r==0){
-                    curr[c]=grid[r][c]+curr[c-1];
-                    continue;
-                }
-                if(c==0){
-                    curr[c]=grid[r][c]+prev[c];
-                    continue;
-                }
-                curr[c]=grid[r][c]+Math.min(curr[c-1],prev[c]);
-            }
-            prev=curr;
-        }
+    private int minpath(int[][] grid,int r, int c,int[][] dp){
+        if(r<0 || c<0) return (int)1e9;
+        if(r==0 && c==0) return grid[r][c];
+        if(dp[r][c]!=-1) return dp[r][c];
         
-        return prev[grid[0].length-1];
+        int up = minpath(grid,r-1,c,dp);
+        int left = minpath(grid,r,c-1,dp);
+        return dp[r][c] = grid[r][c] + Math.min(up,left);
+    }
+    public int minPathSum(int[][] grid) {
+        int m= grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m][n];
+        for(int row[] : dp) Arrays.fill(row,-1);
+        return minpath(grid,m-1,n-1,dp);
     }
 }
