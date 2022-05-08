bfbb43268e7763ec0e5d0bcebc4585c8b3baf27e
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-08 17:52:27 +0530

    Time: 1 ms (95.03%), Space: 40.7 MB (84.70%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -12,8 +12,23 @@ class Solution {
         return dp[i] = count;
     }
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()];
-        Arrays.fill(dp,-1);
-        return numways(s,0,dp);
+        int dp[] = new int[s.length()+1];
+        // Arrays.fill(dp,-1);
+        // return numways(s,0,dp);
+        
+        //base conditions
+        dp[s.length()] = 1;
+        for(int i=s.length()-1;i>=0;i--){
+            int count = 0;
+            count += (s.charAt(i)!='0')?dp[i+1]:0;
+            if(i<s.length()-1){
+                int num = Integer.parseInt(s.substring(i,i+2));
+                count += !(num>9 && num<27)?0:dp[i+2];
+            }
+            dp[i] = count;
+        }
+        return dp[0];
+        
+        
     }
 }
