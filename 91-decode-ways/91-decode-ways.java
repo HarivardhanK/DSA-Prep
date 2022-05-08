7c07763a26df2ac7ba7ca24e5d51adce8bff70ee
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-08 17:56:59 +0530

    Time: 2 ms (61.65%), Space: 42 MB (69.55%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,34 +1,21 @@
 class Solution {
-    private int numways(String s, int i, int dp[]){
-        if(i>=s.length()) return 1;
-        if(s.charAt(i)=='0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int count = 0;
-        count += numways(s,i+1,dp);
-        if(i<s.length()-1){
-            int num = Integer.parseInt(s.substring(i,i+2));
-            count += !(num>9 && num<27)?0:numways(s,i+2,dp);
-        }
-        return dp[i] = count;
-    }
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()+1];
-        // Arrays.fill(dp,-1);
-        // return numways(s,0,dp);
-        
+        // int dp[] = new int[s.length()+1];
+        int next1, next2=0;
         //base conditions
-        dp[s.length()] = 1;
+        // dp[s.length()] = 1;
+        next1=1;
+        
         for(int i=s.length()-1;i>=0;i--){
             int count = 0;
-            count += (s.charAt(i)!='0')?dp[i+1]:0;
+            count += (s.charAt(i)!='0')?next1:0;
             if(i<s.length()-1){
                 int num = Integer.parseInt(s.substring(i,i+2));
-                count += !(num>9 && num<27)?0:dp[i+2];
+                count += !(num>9 && num<27)?0:next2;
             }
-            dp[i] = count;
+            next2=next1;
+            next1 = count;
         }
-        return dp[0];
-        
-        
+        return next1;
     }
 }
