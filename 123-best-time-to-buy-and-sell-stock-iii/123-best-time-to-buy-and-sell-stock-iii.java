38341b6a2057137450e210c00ad5a7a3e6d49f04
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-17 20:25:22 +0530

    Time: 209 ms (10.35%), Space: 116.2 MB (18.15%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int ahead[] = new int[5];
+    private int solve(int[] prices, int ind, int isbuy, int dp[][]){
         
-        for(int i=n-1;i>=0;i--){
-            int curr[] = new int[5];
-            for(int j=3;j>=0;j--){
-                if(j%2==0){
-                    curr[j]=Math.max(-prices[i]+ahead[j+1],ahead[j]);
-                }
-                else{
-                    curr[j]=Math.max(prices[i]+ahead[j+1],ahead[j]);
-                }
-            }
-            ahead = curr;
+        if(ind==prices.length || isbuy==4) return 0;
+        if(dp[ind][isbuy]!=-1) return dp[ind][isbuy];
+        int pick = (int)-1e9, nopick = (int)-1e9;
+        if(isbuy%2 == 0){
+            pick = Math.max( -prices[ind] + solve(prices, ind+1, isbuy+1,dp),solve(prices, ind+1, isbuy,dp));
         }
-            
-        return ahead[0];
+        else{
+            nopick = Math.max(prices[ind] + solve(prices, ind+1, isbuy+1,dp), solve(prices, ind+1, isbuy,dp));
+        }
+        return dp[ind][isbuy] = Math.max(pick, nopick);
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][]=  new int[n][4];
+        for(int r[] : dp)
+            Arrays.fill(r, -1);
+        return solve(prices, 0 , 0, dp);
     }
 }
