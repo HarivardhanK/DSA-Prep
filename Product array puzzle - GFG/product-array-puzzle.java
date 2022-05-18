69de04156cf925dd1b11a46fd45e9244ae2113fd
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-18 14:26:59 +0530

    Added solution - LeetHub

---
+++ Product array puzzle - GFG/product-array-puzzle.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+        	int[] array = new int[n];
+        	for (int i=0; i<n ; i++ ) {
+        		array[i] = sc.nextInt();
+        	}
+            Solution ob = new Solution();
+            long[] ans = new long[n];
+            ans = ob.productExceptSelf(array, n); 
+
+           	for (int i = 0; i < n; i++) { 
+				System.out.print(ans[i]+" ");
+			} 
+            System.out.println();
+            t--;
+        }
+    } 
+} 
+  
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution 
+{ 
+	public static long[] productExceptSelf(int nums[], int n) {
+        long res[] = new long[n];
+        
+        res[0] = 1;
+        for(int i=1;i<n;i++){
+            res[i] = nums[i-1] * res[i-1];
+        }
+        
+        long right = 1;
+        for(int i=n-1;i>=0;i--){
+            res[i] *= right;
+            right *=nums[i];
+        }
+        
+        return res;
+	} 
+} 
