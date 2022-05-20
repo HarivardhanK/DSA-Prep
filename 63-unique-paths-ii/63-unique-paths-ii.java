17e82949c65f596c19d40fd43fa769cf755238ad
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-05-20 16:27:17 +0530

    Time: 1 ms (52.25%), Space: 42 MB (63.73%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,29 +1,21 @@
 class Solution {
+    private int solve(int[][] grid, int r, int c, int dp[][]){
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]==1) return 0;
+        if(r==grid.length-1 && c==grid[0].length-1) return 1;
+        
+        if(dp[r][c]!=-1) return dp[r][c];
+        
+        int count = solve(grid, r+1, c,dp);
+        count += solve(grid, r, c+1,dp);
+        return dp[r][c] = count;
+    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid.length==1 && obstacleGrid[0].length ==1) return obstacleGrid[0][0]^1;
-        if(obstacleGrid[0][0]==1) return 0;
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        int prev[]=new int[n];
-        for(int r=0;r<m;r++){
-            int curr[] = new int[n];
-            for(int c=0;c<n;c++){
-                if(obstacleGrid[r][c]==1){
-                    curr[c]=0;
-                    continue;
-                }
-                if(r==0 && c==0){
-                    curr[c]=1;
-                    continue;
-                }
-                curr[c]=0;
-                if(r>0)
-                    curr[c]+=prev[c];
-                if(c>0)
-                    curr[c]+=curr[c-1];
-            }
-            prev=curr;
-        }
-        return prev[n-1];
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        
+        int dp[][] = new int[m][n];
+        for(int row[]:dp) 
+            Arrays.fill(row, -1);
+        
+        return solve(obstacleGrid, 0, 0, dp);
     }
 }
