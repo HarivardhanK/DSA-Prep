a983bd3b13bcc2689ff64532f4087fd724212cc9
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-02 19:44:40 +0530

    Time: 1 ms (92.88%), Space: 44 MB (82.76%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,25 +1,36 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length,n=matrix[0].length;
-        boolean isfirstcol=false;
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0) isfirstcol=true;
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
+        boolean firstCol = false;
+        int m = matrix.length, n = matrix[0].length;
+        for(int i = 0; i < m; i++) {
+            if( matrix[i][0] == 0) 
+                firstCol = true;
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0) 
-                    matrix[i][j]=0;
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        if(matrix[0][0]==0) Arrays.fill(matrix[0],0);
-        if(isfirstcol){
-            for(int i=0;i<m;i++) matrix[i][0]=0;
+        
+        //first row
+        if(matrix[0][0] == 0)
+            Arrays.fill(matrix[0], 0);
+        
+        //first col
+        if(firstCol){
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
+        
     }
 }
