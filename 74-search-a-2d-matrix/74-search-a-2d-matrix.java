8d1dc76982bc35eba7c5dd94f6a6ccff6d5ce84c
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-07 16:13:20 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (40.54%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,10 +1,33 @@
 class Solution {
+    private boolean binsearch(int[][] matrix, int row, int target) {
+        int s = 0, e = matrix[0].length - 1;
+        while(s <= e) {
+            int mid = (s + e) / 2;
+            if(matrix[row][mid] == target)
+                return true;
+            else if(matrix[row][mid] < target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+        return false;
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int r = matrix.length - 1,c = 0;
-        while(r >= 0 && c < matrix[0].length){
-            if(matrix[r][c]==target) return true;
-            else if(matrix[r][c] > target) r--;
-            else c++;
+       if( matrix.length == 0) return false;
+        
+        int top = 0, down = matrix.length - 1;
+        int col = matrix[0].length - 1;
+        
+        while(top <= down) {
+            int mid = (top + down)/2;
+            //if target is in between [matrix[mid][0], matrix[mid][col]] search in that row
+            if(matrix[mid][0] <= target && matrix[mid][col] >= target)
+                return binsearch(matrix, mid, target);
+            //else divide the array in two
+            else if(matrix[mid][col] < target)
+                top = mid + 1;
+            else 
+                down = mid - 1;
         }
         return false;
     }
