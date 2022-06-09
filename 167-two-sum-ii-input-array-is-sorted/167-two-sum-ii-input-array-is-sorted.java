12af1fec54011c0499e1a2940597ff18785d79f4
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-09 10:10:46 +0530

    Time: 1 ms (99.22%), Space: 44.7 MB (96.62%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -2,11 +2,14 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         //using the two pointers one pointing to the begin and other ot last
         int start = 0, end = numbers.length-1;
-        while(start<end){
-            int sum = numbers[start]+numbers[end];
-            if(sum==target) return new int[]{start+1,end+1};
-            else if(sum<target) start++;
-            else end--;
+        while(start < end){
+            int sum = numbers[start] + numbers[end];
+            if(sum == target)
+                return new int[]{start+1, end+1};
+            else if(sum < target)
+                start++;
+            else
+                end--;
         }
         return numbers;
     }
