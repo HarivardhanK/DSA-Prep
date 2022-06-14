2e0696e6bb1ec77f69435dd9513aa3bf2c05fc22
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-14 10:37:14 +0530

    Time: 25 ms (9.27%), Space: 46.2 MB (69.69%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,23 +1,28 @@
 class Solution {
-    private int lcs(String word1,String word2){
-        int n1=word1.length(),n2=word2.length();
-        int prev[] = new int[n2+1];
-        Arrays.fill(prev,0);
-        for(int i=1;i<=n1;i++){
-            int curr[] = new int[n2+1];
-            for(int j=1;j<=n2;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    curr[j]=1+prev[j-1];
-                }
-                else{
-                    curr[j]=Math.max(prev[j],curr[j-1]);
-                }
-            }
-            prev=curr;
+    private int lcs(String word1, String word2, int ind1, int ind2, int[][] dp) {
+        if(ind1 == word1.length() || ind2 == word2.length())
+            return 0;
+        
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        
+        if(word1.charAt(ind1) == word2.charAt(ind2)) {
+            return dp[ind1][ind2] = 1 + lcs(word1, word2, ind1 + 1, ind2 + 1, dp);
         }
-        return prev[n2];
+        int first = lcs(word1, word2, ind1 + 1, ind2, dp);
+        int second = lcs(word1, word2, ind1, ind2 + 1, dp);
+        return dp[ind1][ind2] = Math.max(first, second);
     }
     public int minDistance(String word1, String word2) {
-        return ((word1.length()+word2.length())-2*(lcs(word1,word2)));
+        //longest common subsequence - lcs
+        //we need to find the lcs of these two stings
+        //w1 = sea and w2 = eat then lcs = 2
+        // answer =  sum of the length of two words - 2 * lcs(w1, w2)
+        
+        int dp[][] = new int[word1.length()][word2.length()];
+        for(int row[]: dp) 
+            Arrays.fill(row, -1);
+        
+        return (word1.length() + word2.length()) - (2 * lcs(word1, word2, 0, 0, dp));
+        
     }
 }
