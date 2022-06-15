32f4b1bc342983c0c5027a97f86c2ffed451414a
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-15 12:20:25 +0530

    Time: 1 ms (97.07%), Space: 44.4 MB (66.07%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,29 +1,24 @@
 class Solution {
-    private void solve(int nums[], int ind, List<List<Integer>> ans){
-        if(ind == nums.length){
+    private void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void solve(int[] nums, int ind, List<List<Integer>> ans) {
+        if(ind == nums.length) {
             List<Integer> arr = new ArrayList<>();
-            for(int ele:nums) arr.add(ele);
+            for(int n: nums) arr.add(n);
             ans.add(arr);
             return;
         }
-        
-        for(int i=ind;i<nums.length;i++){
-            swap(nums,ind,i);
-            solve(nums,ind+1,ans);
-            swap(nums,ind,i);
+        for(int i = ind; i < nums.length; i++) {
+            swap(nums, ind, i);
+            solve(nums, ind + 1, ans);
+            swap(nums, ind, i);
         }
     }
-    private void swap(int nums[], int i, int j){
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
-    }
     public List<List<Integer>> permute(int[] nums) {
-        //Approach 1: use map to keep track of elements that have been taken as constraints are 1 <= nums.length <= 6
-    
-        //Appraoch 2: use swap, swap the i in for (ind to n-1) 
-        // if ind == nums.length ans into the ans
-        
+        //using swaps
         List<List<Integer>> ans = new ArrayList<>();
         solve(nums, 0, ans);
         return ans;
