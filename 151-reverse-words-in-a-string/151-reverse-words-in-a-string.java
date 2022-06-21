70b1be9a27953423fa2274e7b8b4e93dfe064007
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-21 19:27:19 +0530

    Time: 5 ms (87.84%), Space: 43.4 MB (61.59%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,11 +1,52 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] str= s.split(" ");
-        StringBuilder sb = new StringBuilder();
-        sb.append(str[str.length-1].trim());
-        for(int i=str.length-2;i>=0;i--){
-            if(str[i].length()>0) sb.append(" "+str[i].trim());
-        }
-        return sb.toString();
+        if(s == null) 
+            return s;
+        
+        char arr[] = s.toCharArray();
+        int n = arr.length;
+        
+        //reverse the whole string
+        reverse(arr, 0, n - 1);
+        
+        //reverse the words 
+        reverseWords(arr, n);
+        
+        //clean ups - removes the trailing and multiple spaces in the string
+        return cleanup(arr, n);
+        
     }
+    private void reverse(char[] arr, int i, int j) {
+        while(i < j) {
+            char ch = arr[i];
+            arr[i] = arr[j];
+            arr[j] = ch;
+            i++; j--;
+        }
+    }
+    private void reverseWords(char[] arr, int n) {
+        int i = 0, j = 0;
+        while(i < n) {
+            while(i<j || (i<n && arr[i] == ' '))
+                i++;
+            while(j<i || (j<n && arr[j] != ' '))
+                j++;
+            reverse(arr, i, j - 1);
+        }
+    }
+    private String cleanup(char[] arr, int n) {
+        int i = 0, j = 0;
+        while(j < n) {
+            while(j<n && arr[j]==' ') 
+                j++;
+            while(j<n && arr[j]!=' ') 
+                arr[i++] = arr[j++];
+            while(j<n && arr[j]==' ') 
+                j++;
+            if(j<n) 
+                arr[i++] = ' ';
+        }
+        return new String(arr).substring(0, i);
+    }
+    
 }
