6b3ef70ce7bf1d2027bca0da422b1fd45e537353
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-06-22 23:05:02 +0530

    Time: 14 ms (59.86%), Space: 74.1 MB (57.98%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+// which stores the lowerbound, upperbound and maxsum
 class NodeValue {
     int minNode, maxNode, maxSize;
     NodeValue(int min, int max, int size) {
@@ -22,29 +23,33 @@ class NodeValue {
     }
 }
 class Solution {
+// global variables
     int MIN = Integer.MIN_VALUE, MAX = Integer.MAX_VALUE;
     int maxsum = 0;
+
     private NodeValue solve(TreeNode root) {
         if(root == null) 
             return new NodeValue(MAX, MIN, 0);
-        
+
+        //post order traversal
         NodeValue left = solve(root.left);
         NodeValue right = solve(root.right);
-        
+
+                //not a valid BST
         if(!(left != null && right != null && root.val > left.maxNode && root.val < right.minNode)) 
             return null;
-        
+
+                // update the maximum sum
         int maxi = root.val + left.maxSize + right.maxSize;
         maxsum = Math.max(maxi, maxsum);
+
+                //update the lower bound and the upper bound of the current node
         int min = Math.min(root.val, left.minNode);
         int max = Math.max(root.val, right.maxNode);
+
         return new NodeValue(min, max, maxi);
     }
-    /* 
-        simple rule is that
-        if largest node on left subtree is less than root 
-        if smallest node on right subtree is greater then root then it is a BST
-    */
+
     public int maxSumBST(TreeNode root) {
         solve(root);
         return maxsum;
