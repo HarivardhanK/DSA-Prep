77612c9e7bccd640d63b8219fa8406663569bff5
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-07-04 19:16:58 +0530

    Time: 7 ms (21.75%), Space: 51.5 MB (60.94%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,23 +1,21 @@
 class Solution {
     public int candy(int[] ratings) {
-        int candies[] = new int[ratings.length];
-        Arrays.fill(candies,1);
+        int[] ans = new int[ratings.length];
+        Arrays.fill(ans, 1);
         
-        //left to right
-        for(int i=1;i<ratings.length;i++){
-            if(ratings[i-1]<ratings[i]){
-                candies[i] = 1+candies[i-1];
+        for(int i = 1; i<ratings.length; i++) {
+            if(ratings[i] > ratings[i - 1]) {
+                ans[i] = ans[i-1] + 1;
             }
         }
-        //further last index will not change
-        int sum= candies[candies.length-1];
-
-        //right to left
-        for(int i=ratings.length-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                candies[i] = Math.max(candies[i],1 + candies[i+1]);
+        for(int j = ratings.length -2; j >= 0; j--) {
+            if(ratings[j] > ratings[j + 1]) {
+                ans[j] = Math.max(ans[j], 1 + ans[j + 1]);
             }
-            sum+=candies[i];
+        }
+        int sum = 0;
+        for(int ele: ans) {
+            sum += ele;
         }
         return sum;
     }
