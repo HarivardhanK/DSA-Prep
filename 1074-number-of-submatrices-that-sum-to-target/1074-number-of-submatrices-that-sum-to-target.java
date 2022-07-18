aa8d75dfc246b838b98d3bb3d6d8cfa0b1dd7888
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-07-18 19:57:01 +0530

    Time: 187 ms (77.92%), Space: 42.9 MB (96.28%) - LeetHub

---
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        
+        //prefix sum for each row in the matrix
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+        
+        int count = 0;
+        //now for each and every column we find the submats sum to k
+        for(int c1 = 0; c1 < n; c1++) {
+            for(int c2 = c1; c2 < n; c2++) {
+                Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+                map.put(0, 1);
+                int sum = 0;
+                
+                for(int r = 0; r < m; r++) {
+                    sum += matrix[r][c2] - (c1 > 0? matrix[r][c1 - 1]: 0);
+                    count += map.getOrDefault(sum - target, 0);
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                }
+            }
+        }
+        return count;
+    }
+}
