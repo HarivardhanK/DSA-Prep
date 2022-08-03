b253a2c250b150f62fda9531a8fa9216080e5a00
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-08-03 20:04:47 +0530

    Time: 45 ms (41.04%), Space: 55.9 MB (7.26%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,16 +1,25 @@
 class MyCalendar {
-    List<int[]> arr;
+    // pair of (start, end) 
+    //used a treemap because it is a balanced, red-black tree and insertion and searching is O(logn)
+    //which optimises our solution
+    TreeMap<Integer, Integer> treemap; 
     public MyCalendar() {
-        arr = new ArrayList<>();
+        treemap = new TreeMap<>();
     }
     
     public boolean book(int start, int end) {
-        for(int[] node: arr) {
-            // if((node[0] <= start && start < node[1]) || (node[0] < end && end <= node[1]))
-            if(node[0] < end && start < node[1])
-                return false;
-        }
-        arr.add(new int[]{start, end});
+        
+        //condition for start
+        //if start < lower entry end
+        if(treemap.floorEntry(start) != null && start < treemap.floorEntry(start).getValue()) 
+            return false;
+        
+        //condtion for end
+        //if Higher entry start < end
+        if(treemap.ceilingEntry(start) != null && treemap.ceilingEntry(start).getKey() < end)
+            return false;
+        
+        treemap.put(start, end);
         return true;
     }
 }
@@ -19,5 +28,4 @@ class MyCalendar {
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar obj = new MyCalendar();
  * boolean param_1 = obj.book(start,end);
- [null,true,true,false,false,true,false,true,true,true,false]
  */
