c0dc3382855fed8f394368848cc57e9e5b9fe024
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-08-08 18:51:16 +0530

    Time: 1200 ms (5.01%), Space: 256.8 MB (5.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,26 +1,34 @@
 class Solution {
-    private int binsearch(List<Integer> arr, int s, int e,int target){
-        while(s<e){
-            int mid=s+(e-s)/2;
-            if(arr.get(mid)==target) return mid;
-            else if(arr.get(mid)<target) s++;
-            else e--;
+    private int solve(int[] nums, int i, int prev) {
+        if(i >= nums.length) return 0;
+        
+        if(prev == -1 || nums[prev] < nums[i]) {
+            return Math.max(1+solve(nums, i+1, i), solve(nums,i+1, prev));
         }
-        return s;
+        return solve(nums, i+1, prev);
+    }
+    // here there are two changing parameters so it is 2d matrix
+    private int solveMem(int[] nums, int i, int prev, int[][] dp) {
+        if(i >= nums.length) return 0;
+        
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+        
+        if(prev == -1 || nums[prev] < nums[i]) {
+            return dp[i][prev+1] = Math.max(1+solveMem(nums, i+1, i,dp), solveMem(nums,i+1, prev,dp));
+        }
+        return dp[i][prev+1] = solveMem(nums, i+1, prev,dp);
     }
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i]>arr.get(arr.size()-1)){
-                arr.add(nums[i]);
-            }
-            else{
-                int ind = binsearch(arr,0,arr.size()-1,nums[i]);
-                arr.set(ind,nums[i]);
-            }
-        }
-        return arr.size();
+        // return solve(nums, 0, -1);
+        
+        
+        // here we took the value of prev as -1 in array there 
+        // is no -1 so increment prev's value and also i value
+        
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] row: dp) Arrays.fill(row, -1);
+        
+        return solveMem(nums, 0, -1, dp);
     }
 }
