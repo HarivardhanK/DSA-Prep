59c029cba85f737da3a4ffe52db3c12938184d14
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-09-14 12:41:33 +0530

    Time: 13 ms (89.47%), Space: 104.4 MB (83.63%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -14,26 +14,21 @@
  * }
  */
 class Solution {
-    private int solve(TreeNode root, int[] ar) {
-        if(root == null) return 0;
-        if(root != null && root.left == null && root.right == null) {
-            // System.out.println(Arrays.toString(ar));
-            ar[root.val]++;
-            int odds = 0;
-            for(int i = 0;i < 10; i++) {
-                if((ar[i]&1) == 1) odds++;
+    int count = 0;
+    private void preorder(TreeNode root, int path) {
+        if(root != null) {
+            path = path ^ (1<<root.val);
+            
+            if(root.left == null && root.right == null) {
+                if((path&(path-1)) == 0) 
+                    count++;
             }
-            ar[root.val]--;
-            if(odds <= 1) return 1;
-            return 0;
+            preorder(root.left, path);
+            preorder(root.right, path);
         }
-        ar[root.val]++;
-        int ans = solve(root.left, ar);
-        ans += solve(root.right, ar);
-        ar[root.val]--;
-        return ans;
     }
     public int pseudoPalindromicPaths (TreeNode root) {
-        return solve(root, new int[10]);
+        preorder(root, 0);
+        return count;
     }
 }
