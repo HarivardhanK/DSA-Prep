0e4373a04745e9688b40487332b50d7608cf064d
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-10-01 12:01:21 +0530

    Time: 1 ms (98.72%), Space: 41 MB (88.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int numDecodings(String s) {
-        // int dp[] = new int[s.length()+1];
-        int next1, next2=0;
-        //base conditions
-        // dp[s.length()] = 1;
-        next1=1;
-        
-        for(int i=s.length()-1;i>=0;i--){
-            int count = 0;
-            count += (s.charAt(i)!='0')?next1:0;
-            if(i<s.length()-1){
-                int num = Integer.parseInt(s.substring(i,i+2));
-                count += !(num>9 && num<27)?0:next2;
-            }
-            next2=next1;
-            next1 = count;
+    private int numdec(String s, int i, int[] dp) {
+        if(i >= s.length()) return 1;
+        if(dp[i] != -1) return dp[i];
+        int count = 0;
+        if(s.charAt(i) >= '1' && s.charAt(i) <= '9') 
+            count = numdec(s, i+1, dp);
+        if(i+1 < s.length() && s.charAt(i) != '0') {
+            int num = Integer.parseInt(s.substring(i,i+2));
+            if(num >= 10 && num <= 26) 
+                count += numdec(s, i+2, dp);
         }
-        return next1;
+        return dp[i] = count;
+    }
+    public int numDecodings(String s) {
+        int dp[] = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return numdec(s, 0, dp);
     }
 }
