f54a01fd4529f32dfc5281f84623ada492f2f4c5
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-10-03 12:59:56 +0530

    Time: 16 ms (49.37%), Space: 88.7 MB (83.54%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,37 +1,14 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        // int n = neededTime.length, currmax =-1, ans = 0;
-        // int[] presum = new int[n];
-        // if(n > 1 && colors.charAt(0) == colors.charAt(1)) {
-        //     currmax = neededTime[0];
-        //     presum[0] = currmax;
-        // }
-        
-        
         int n = neededTime.length;
-        int currmax = 0, currsum = 0, mintime = 0;
+        int currmax = 0, mintime = 0;
         for(int i = 0; i < n; i++) {
-            if(i > 0 && i+1 < n && colors.charAt(i) == colors.charAt(i+1) && colors.charAt(i-1) != colors.charAt(i)) {
-                mintime += currsum - currmax;
-                currsum = 0;
+            if(i > 0 && colors.charAt(i) != colors.charAt(i-1)) 
                 currmax = 0;
-            }
-            if(i>0 && colors.charAt(i)==colors.charAt(i-1)) {
-                currmax = Math.max(currmax, neededTime[i]);
-                currsum += neededTime[i];
-            }
-            else if(i+1<n && colors.charAt(i)==colors.charAt(i+1)) {
-                currmax = Math.max(currmax, neededTime[i]);
-                currsum += neededTime[i];
-            }
-            else {
-                mintime += currsum - currmax;
-                currsum = 0;
-                currmax = 0;
-            }
+            
+            mintime += Math.min(currmax, neededTime[i]);
+            currmax = Math.max(currmax, neededTime[i]);
         }
-        mintime += currsum - currmax;
-        
         return mintime;
     }
 }
