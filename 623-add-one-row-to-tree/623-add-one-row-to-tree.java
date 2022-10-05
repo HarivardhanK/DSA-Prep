80936ed5f3b77e1d37e1152ebaeda50214ac3aa7
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-10-05 14:04:13 +0530

    Time: 1 ms (88.59%), Space: 45.1 MB (66.78%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -15,16 +15,13 @@
  */
 class Solution {
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        if(root==null) return root;
-        if(depth == 1){
-            TreeNode newNode = new TreeNode(val,root,null);
-            return newNode;
+        if(root == null) return null;
+        if(depth == 1) {
+            return new TreeNode(val, root, null);
         }
-        if(depth == 2){
-            TreeNode newNodeleft = new TreeNode(val,root.left,null);
-            TreeNode newNoderight = new TreeNode(val,null,root.right);
-            root.left = newNodeleft;
-            root.right = newNoderight;
+        if(depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
             return root;
         }
         root.left = addOneRow(root.left, val, depth-1);
