fc6ad7e7dce7fa10ae32be6802ff5282097fbe69
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-11-07 18:39:44 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for JAVA
 
 import java.util.*;
@@ -30,6 +30,7 @@ class GFG
         }
     }
 }
+
 // } Driver Code Ends
 
 
@@ -39,18 +40,16 @@ class Solution
 {
     public void shortest_distance(int[][] matrix)
     {
-        int m = matrix.length, n = matrix[0].length;
-        for(int k = 0; k < m; k++) {
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(matrix[i][k] == -1 || matrix[k][j] == -1) 
-                        continue;
-                    else if(matrix[i][j] == -1){
+        int n = matrix.length, m = matrix[0].length;
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    if(matrix[i][k] == -1 || matrix[k][j] == -1) continue;
+                    
+                    if(matrix[i][j] == -1) 
                         matrix[i][j] = matrix[i][k] + matrix[k][j];
-                    }
-                    else {
-                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
-                    }
+                    else
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k]+matrix[k][j]);
                 }
             }
         }
